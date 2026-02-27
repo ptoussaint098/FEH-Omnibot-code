472d217e4ddc841c8c23708d97b4fb919045ad5a
ptoussaint098 <ptoussaint098@gmail.com>
2026-02-27 08:58:27 -0500

    added new erc2 template code

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <FEH.h>
+#include <Arduino.h>
+#include <stdio.h>
+
+
+// Declare things like Motors, Servos, etc. here
+// For example:
+// FEHMotor leftMotor(FEHMotor::Motor0, 6.0);
+// FEHServo servo(FEHServo::Servo0);
+
+
+void ERCMain()
+{
+    // Your code here!
+
+    // Or just use the TestGUI function
+    TestGUI();
+
+}
