0a4a192d31d14a123ce3ae0feb43b0674950b23b
ptoussaint098 <ptoussaint098@gmail.com>
2026-02-27 15:00:51 -0500

    Removed Comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,9 @@
 #include <stdio.h>
 
 
-// Declare things like Motors, Servos, etc. here
-// For example:
-// FEHMotor leftMotor(FEHMotor::Motor0, 6.0);
-// FEHServo servo(FEHServo::Servo0);
 
 
 void ERCMain()
 {
-    // Your code here!
-
-    //test change for branch
-    // bruh]
-    //help
     
-
-
-    
-    // Or just use the TestGUI function
-    TestGUI();
-
 }
