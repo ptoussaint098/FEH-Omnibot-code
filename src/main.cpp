45ba65167eeae6ebe96fba9e8c0f21af3c8d69a8
ptoussaint098 <ptoussaint098@gmail.com>
2026-04-28 09:53:00 -0400

    Final Code fr

--- src/main.cpp
+++ src/main.cpp
@@ -52,23 +52,29 @@ FEHServo compost(FEHServo::Servo7);
 
 class robot{
     public:
+    //accepts arguements for degrees at any angle relative to robot
+    //dist is in inches
+    //speed is in inches per second
     void move(float dist, float angle, float speed)
     {
-        FEHLog::printf("Entered loop");
+       //RESET 
         encoder1.ResetCounts();
         encoder2.ResetCounts();
         encoder3.ResetCounts();
+        //for move function leftover code
+        loop_speed=speed;
 
+        //calculates a relative velocity
         velocity_x=cos((angle/180.0)*Pi)*speed;
         velocity_y=sin((angle/180.0)*Pi)*speed;
 
-       
+       //calculates correct wheel velocities
         wheelspeedcalc(velocity_x,velocity_y, 0);
-
+        //calculates encoder counts from time and wheelspeed
         encoder1dstcount=((((fabs(wheelspeedrpm1)*wheel_radius*(2.0*Pi))/60)*(dist/speed))*countsperinch);
         encoder2dstcount=((((fabs(wheelspeedrpm2)*wheel_radius*(2.0*Pi))/60)*(dist/speed))*countsperinch);
         encoder3dstcount=((((fabs(wheelspeedrpm3)*wheel_radius*(2.0*Pi))/60)*(dist/speed))*countsperinch);
-
+        //ensures that if the encount is low then it will not be as low since this exit the loop early from previous movement
         if (encoder1dstcount<15)
         {
             encoder1dstcount+=20;
@@ -83,13 +89,13 @@ class robot{
         }
 
        
-
+        //resets the PID variables from previos calls of the move function
         pidreset();
        
-
+        //loop unti la wheel reaches the correct destination
         while ((encoder1.Counts()<=encoder1dstcount)&&(encoder2.Counts()<=encoder2dstcount)&&(encoder3.Counts()<=encoder3dstcount))
         {
-
+            //sets the motors to the adequete voltage
             motor1.SetPercent(motor1_voltage);
             motor2.SetPercent(motor2_voltage);
             motor3.SetPercent(motor3_voltage);
@@ -98,14 +104,15 @@ class robot{
 
    
     }
+    //calculates corect wheelspeed for each wheel, should not be called in main
     void wheelspeedcalc(float vx, float vy, float botrot)
     {
-       
+       //calculates the rpm of each wheel
         wheelspeedrpm1 = ((sin((wheel1_theta/360.0)*2.0*Pi)*vx+-1.0*cos((wheel1_theta/360.0)*2.0*Pi)*vy+(-1.0*robot_radius*botrot))/wheel_radius)*(60.0/(2.0*Pi));
         wheelspeedrpm2 = ((sin((wheel2_theta/360.0)*2*Pi)*vx+-1.0*cos((wheel2_theta/360.0)*2.0*Pi)*vy+(-1.0*robot_radius*botrot))/wheel_radius)*(60.0/(2.0*Pi));
         wheelspeedrpm3 = ((sin((wheel3_theta/360.0)*2.0*Pi)*vx+-1.0*cos((wheel3_theta/360.0)*2.0*Pi)*vy+(-1.0*robot_radius*botrot))/wheel_radius)*(60.0/(2.0*Pi));
 
-
+        //calculates correct voltage for each wheel based on the current max rpm under load
         motor1_voltage = ((wheelspeedrpm1/motormaxrpm)*100);
         motor2_voltage = ((wheelspeedrpm2/motormaxrpm)*100);
         motor3_voltage = ((wheelspeedrpm3/motormaxrpm)*100);
@@ -113,42 +120,52 @@ class robot{
 
 
     }
+    //calculates the PID should not be called in main
     void pidcalc()
     {
+        //finds the time between calls
         time_next_pid=millis();
         time_diff_pid=time_next_pid-last_time_pid;
 
+        //sets the current encoder counts
         encoder1now=encoder1.Counts();
         encoder2now=encoder2.Counts();
         encoder3now=encoder3.Counts();
 
+
+        //finds the wheel speed based on the encoders
         actual_wheel_speed1=((encoder1now-encoder1last)/countsperinch)/(time_diff_pid*.001);
         actual_wheel_speed2=((encoder2now-encoder2last)/countsperinch)/(time_diff_pid*.001);
         actual_wheel_speed3=((encoder3now-encoder3last)/countsperinch)/(time_diff_pid*.001);
 
+        //finds the actual individual wheel RPM which is mostly for troubleshooting
         actualwheelspeedrpm1=(actual_wheel_speed1*60.0)/(2*Pi);
         actualwheelspeedrpm2=(actual_wheel_speed2*60.0)/(2*Pi);
         actualwheelspeedrpm3=(actual_wheel_speed3*60.0)/(2*Pi);
 
-
+        //finds error between actual and projected
         pid_error1=((fabs(wheelspeedrpm1)*Pi*wheel_radius*2.0)/60.0)-actual_wheel_speed1;
         pid_error2=((fabs(wheelspeedrpm2)*Pi*wheel_radius*2.0)/60.0)-actual_wheel_speed2;
         pid_error3=((fabs(wheelspeedrpm3)*Pi*wheel_radius*2.0)/60.0)-actual_wheel_speed3;
 
+        //finds error in rpm which is for tourbleshooting
         pid_errorrpm1= (pid_error1*60)/(Pi*wheel_radius*2.0);
         pid_errorrpm2= (pid_error2*60)/(Pi*wheel_radius*2.0);
         pid_errorrpm3= (pid_error3*60)/(Pi*wheel_radius*2.0);
 
+        //finds the error in the voltage which is alspo for trouble shooting
         pid_volterr1=((pid_errorrpm1/motormaxrpm)*100.0);
         pid_volterr2=((pid_errorrpm2/motormaxrpm)*100.0);
         pid_volterr3=((pid_errorrpm3/motormaxrpm)*100.0);
 
         FEHLog::printf("error %f", (double)pid_volterr3);
 
+        //finds the sum of the errors
         pid_sumoferrors1+=pid_volterr1*(time_diff_pid*.001);
         pid_sumoferrors2+=pid_volterr2*(time_diff_pid*.001);
         pid_sumoferrors3+=pid_volterr3*(time_diff_pid*.001);
 
+        //calculates corresponding P, I, and D terms
         pid_Pterm1= Pid_P_Constant*pid_volterr1;
         pid_Pterm2= Pid_P_Constant*pid_volterr2;
         pid_Pterm3= Pid_P_Constant*pid_volterr3;
@@ -161,7 +178,7 @@ class robot{
         pid_Dterm2=Pid_D_Constant*((pid_volterr2-pid_lasterror2)/(time_diff_pid*.001));
         pid_Dterm3=Pid_D_Constant*((pid_volterr3-pid_lasterror3)/(time_diff_pid*.001));
        
-       
+       //Uses Terms for corrections
         motor1_voltage=pid_Pterm1+pid_Iterm1+pid_Dterm1+base_voltage1;
         motor2_voltage=pid_Pterm2+pid_Iterm2+pid_Dterm2+base_voltage2;
         motor3_voltage=pid_Pterm3+pid_Iterm3+pid_Dterm3+base_voltage3;
@@ -179,6 +196,7 @@ class robot{
         pid_lasterror3=pid_volterr3;
 
     }
+    //Simply resets PID to be used for new calls should not be called within main and should be called before move loops
     void pidreset()
     {
         time_next_pid=0;
@@ -208,20 +226,23 @@ class robot{
    
         last_time_pid=millis();
     }
+    //Function to turn by any degree in radians for a set period of time
+    //For the degree negative is clockwise, positive is counter clockwise for the robot.
     void turn(float degree, float time)
     {
         encoder1.ResetCounts();
         encoder2.ResetCounts();
         encoder3.ResetCounts();
-
+        //calculates the radians per second for rotation
         radpersec=((Pi*degree)/180)/time;
-
+        //calls wheelspeed calc for said rotation
         wheelspeedcalc(0,0,radpersec);
-
+        //calculates correct encoder amount
         encoder1dstcount=((((fabs(wheelspeedrpm1)*wheel_radius*(2.0*Pi))/60)*(time))*countsperinch)-momentumfactor*1.0;
         encoder2dstcount=((((fabs(wheelspeedrpm2)*wheel_radius*(2.0*Pi))/60)*(time))*countsperinch)-momentumfactor*1.0;
         encoder3dstcount=((((fabs(wheelspeedrpm3)*wheel_radius*(2.0*Pi))/60)*(time))*countsperinch)-momentumfactor*1.0;
 
+        //ensures that momentum doesnt trigger encoder counts
         if (encoder1dstcount==15)
         {
             encoder1dstcount+=15;
@@ -236,7 +257,7 @@ class robot{
         }
 
 
-
+        //main loop until one reaches desired rotation
         while ((encoder1.Counts()<=encoder1dstcount)&&(encoder2.Counts()<=encoder2dstcount)&&(encoder3.Counts()<=encoder3dstcount))
         {
            
@@ -250,6 +271,7 @@ class robot{
        
 
     }
+    //purely experimental function does not work due to RCS Error never call
     void gotopos(float x_pos, float y_pos, float heading)
     {
         RCSPose* class_position= RCS.RequestPosition();
@@ -290,11 +312,16 @@ class robot{
        
 
     }
+    //Moves the arm to a specific angle in a period of time
+    //0 degrees is up no degree greater than 120 should be called
     void armmove(float angle, float time_to_complete)
     {
+        //finds how much arm should move
         waitime=time_to_complete/(abs(angle-arm_angle));
+        //Fixes angle
         if (arm_angle<angle)
         {
+            //Moves arm until it reaches correct position
             while (arm_angle<angle)
             {
                 arm_angle+=1.0;
@@ -305,6 +332,7 @@ class robot{
             }
            
         }
+        //Corrects for opposite movement
         else
         {
              while (arm_angle>angle)
@@ -318,6 +346,7 @@ class robot{
         }
        
     }
+    //Purely used to allow for parallel code execution stops motors
     void stopmot()
     {
        
@@ -341,6 +370,40 @@ class robot{
         SD.FPrintf(motordatapntr,"%f\n%f\n%f\n",motor1_voltage,motor2_voltage,motor3_voltage);
 
     }
+    //Purely experimental does not work due to RCS error
+    void drivetill(float y_posapple)
+    {   encoder1.ResetCounts();
+        encoder2.ResetCounts();
+        encoder3.ResetCounts();
+        RCSPose *rcs_position_loop=RCS.RequestPosition();
+
+        while ((rcs_position_loop->y < y_posapple))
+        {
+            encoder1dstcount=((((fabs(wheelspeedrpm1)*wheel_radius*(2.0*Pi))/60)*(.2/loop_speed))*countsperinch);
+            encoder2dstcount=((((fabs(wheelspeedrpm2)*wheel_radius*(2.0*Pi))/60)*(.2/loop_speed))*countsperinch);
+            encoder3dstcount=((((fabs(wheelspeedrpm3)*wheel_radius*(2.0*Pi))/60)*(.2/loop_speed))*countsperinch);
+            while ((encoder1.Counts()<=encoder1dstcount)&&(encoder2.Counts()<=encoder2dstcount)&&(encoder3.Counts()<=encoder3dstcount))
+            {
+
+
+            }
+            encoder1.ResetCounts();
+            encoder2.ResetCounts();
+            encoder3.ResetCounts();
+            rcs_position_loop=RCS.RequestPosition();
+            if (RCS.RequestsRemaining()<=25)
+            {
+                return;
+            }
+
+        }
+
+        
+
+        
+
+    }
+    //list of Vars
     private:
     float wheelspeed1, wheelspeed2, wheelspeed3;
     float motor1_voltage, motor2_voltage, motor3_voltage;   //in percent
@@ -369,6 +432,7 @@ class robot{
 
     float pid_errorrpm1,pid_errorrpm2,pid_errorrpm3;
     float pid_volterr1, pid_volterr2, pid_volterr3;
+    float loop_speed;
 
 
 };
@@ -381,23 +445,30 @@ class robot{
 
 void ERCMain()
 {
+    //Vars used in Main
+    int check=1;
     int x_touch, y_touch;
+    float applebucket_y;
     char region_char;
     int region;
 
+    //Enables output
     FEHLog::enableBLE(130);
+    //initializes RCS
     RCS.InitializeTouchMenu("0910B7XJM");
     
+    //Set Servo Min and Max
     arm.SetMax(SERVO_MAX);
     arm.SetMin(SERVO_MIN);
-    LCD.WriteLine("program started");
+    
     robot robot;
    
-
+    //Tests SD Card which rarely works
     FEHFile *filepntr = SD.FOpen("Test.txt","w");
     SD.FPrintf(filepntr,"Test");
     FEHLog::printf("Test");
     SD.FCloseAll();
+    //FInds which region we are on
     region_char=RCS.CurrentRegionLetter();
     switch (region_char) {
         case 'A':
@@ -425,16 +496,20 @@ void ERCMain()
             region = 8;
             break;
     }
+    
+    
+
+    //Waits until robot is positioned and button is pressed
     WaitForFinalAction();
 
     
-
+    //Waits until the start light turns red
     while ((cds_cell.Value())>1.2)
     {
         Sleep(50);
     }
 
-
+    //pushes button and moves to compost
     robot.move(3,30,10);
     robot.stopmot();
     robot.move(9,300,10);
@@ -443,6 +518,7 @@ void ERCMain()
     robot.move(.01,180,4);
     robot.move(4.5,270,10);
 
+    //rotates the compost
     robot.stopmot();
     // robot.move(1,270,16);
     // robot.move(1,270,10);
@@ -455,24 +531,28 @@ void ERCMain()
 
     compost.Off();
     robot.stopmot();
+    //restes position and drives to apple bucket
     robot.move(4,270,7);
+    
 
 
 
 
     robot.move(3.5,90,7);
+    robot.move(4,0,5);
     if (region==7)
     {
-        robot.move(8.2,180,10);
+        robot.move(8.5,180,10);
     }
     else
     {
-        robot.move(8.0,180,10);
+        robot.move(8.3,180,10);
     }
     
-
-    
+    //apple bucket pickup and drive to window
     robot.armmove(39,.6);
+
+
     robot.stopmot();
     robot.turn(-162,1.25);
     
@@ -485,11 +565,11 @@ void ERCMain()
 
     Sleep(.5);
     
-    robot.move(10.5,187,12);
+    robot.move(10.5,191,12);
     
     
     int window = RCS.isWindowOpen();
-    
+    //open window and line up with window
     robot.move(12.5,90,12);
     robot.move(2.5,270,5);
     robot.move(8,330,14);
@@ -497,16 +577,18 @@ void ERCMain()
     robot.turn(-125,1);
     robot.move(30,180,12);
     robot.move(3,180,5);
-    robot.move(2.5,0,7);
-    robot.turn(-155,2);
+    robot.move(2.4,0,7);
+    robot.turn(-160,2);
 
     robot.move(47,59,17.25);
     robot.stopmot();
     Sleep(500);
+    //Dropoff apple bucket
     robot.move(1,240,7);
     robot.stopmot();
     robot.armmove(20,.5);
 
+    //Complete humidifier button press
     robot.move(2.1,235,10);
     robot.turn(180,1.25);
     robot.move(5.9,180,12);
@@ -520,6 +602,7 @@ void ERCMain()
         robot.move(16.25,0,12);
     }
     LCD.WriteLine(cds_cell.Value());
+    //Checks color of humidifer button indicator
      while (cds_cell.Value()>2.2)
     {
         Sleep(10);
@@ -542,6 +625,7 @@ void ERCMain()
 
    int lever= RCS.GetLever();
     robot.move(4,0,7);
+    //checks which lever to press and presses said lever
     if (lever==1)
     {
         robot.turn(-105,2);
@@ -554,6 +638,7 @@ void ERCMain()
     {
         robot.turn(-107,2);
     }
+    //move to lever
     robot.move(14.75,60,10);
     robot.armmove(0,.5);
     robot.stopmot();
@@ -570,9 +655,11 @@ void ERCMain()
     robot.armmove(40,.1);
     robot.move(14,240,10);
     robot.armmove(0,.1);
+    //move back from lever and reset position
     robot.turn(105,1);
     robot.move(6,180,7);
     robot.move(9.5,0,10);
+    //close window
     robot.turn(-90,.5);
     robot.move(5,180,5);
     robot.move(18.5,90,12);
@@ -582,9 +669,10 @@ void ERCMain()
     robot.move(25,180,10);
     robot.move(3,0,8);
     robot.turn(90,1);
-    robot.move(40,0,14);
-    robot.turn(30,.5);
-    robot.move(12,0,14);
+    //return to start and press final button
+    robot.move(45,0,14);
+    robot.move(4,180,7);
+    robot.move(12,90,14);
 
 
 
